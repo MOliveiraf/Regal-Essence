07b81c9ad2e59e99d61da5d5b589c6e27eff6758
MOliveiraf <miqueias.oliveiraengmec@gmail.com>
2025-10-05 09:56:13 -0300

    feat: add Product entity and integrate with application layers

--- src/main/java/com/example/demo/config/TestConfig.java
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.example.demo.config;
 
 import com.example.demo.entities.Category;
 import com.example.demo.entities.Order;
+import com.example.demo.entities.Product;
 import com.example.demo.entities.User;
 import com.example.demo.entities.enums.OrderStatus;
 import com.example.demo.repositories.CategoryRepository;
 import com.example.demo.repositories.OrderRepository;
+import com.example.demo.repositories.ProductRepository;
 import com.example.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -26,6 +28,8 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -34,7 +38,14 @@ public class TestConfig implements CommandLineRunner {
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
 
+        Product p1 =  new Product(null, "The Lord of the Rings", "Loren ipsum", 90.5, "");
+        Product p2 =  new Product(null, "Smart TV", "Nulla eu imprerdiet", 2190.0, "");
+        Product p3 =  new Product(null, "Macbook Pro", "Nam eleifend tortor", 1250.32, "");
+        Product p4 =  new Product(null, "PC Gamer", "Donec aliquet odio", 1200.0, "");
+        Product p5 =  new Product(null, "Rails for Dummies", "Cras fringilla convallis", 100.99, "");
+
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "984845454", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "9123335454", "123456");
